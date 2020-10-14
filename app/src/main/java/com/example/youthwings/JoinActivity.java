607dbc6e7d1f4eb96dd067c2dd504db7b25cc2ab
ParkChaeyeong0 <0726tkfkdgo@naver.com>
2020-10-14 23:45:43 +0900

    회원가입 화면 배치 및 툴바 뒤로가기 적용

--- app/src/main/java/com/example/youthwings/JoinActivity.java
+++ app/src/main/java/com/example/youthwings/JoinActivity.java
@@ -1,10 +1,13 @@
 package com.example.youthwings;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.ActionBar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.MenuItem;
 
 public class JoinActivity extends AppCompatActivity {
 
@@ -12,6 +15,25 @@ public class JoinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        //툴바
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false); //기존 title 지우기
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //뒤로가기 버튼
+
+    }
+
+    //툴바
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item ){
+        switch(item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void onClick(View view) {
