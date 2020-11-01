097d8c051f628d1b7ddd6a0a19d45bd882d98a37
kff11 <kff11@naver.com>
2020-11-01 23:13:18 +0900

    서버 URL 주소 변경

--- app/src/main/java/com/example/youthwings/server/RetrofitConnector.java
+++ app/src/main/java/com/example/youthwings/server/RetrofitConnector.java
@@ -7,11 +7,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitConnector {
     final static String URL = "http://studylog.shop:5001/";     // 서버 주소
-    final static String TEST_URL = "http://192.168.0.8:5001/";     // 서버 주소
+    final static String TEST_URL = "http://192.168.0.8:5001/";     // 테스트 서버 주소 (사용하지 마삼)
 
     public static Retrofit createRetrofit() {
         return new Retrofit.Builder()
-                .baseUrl(TEST_URL)
+                .baseUrl(URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
