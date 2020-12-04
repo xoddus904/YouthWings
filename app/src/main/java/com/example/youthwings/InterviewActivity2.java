c54a94cf9d63c63408a5249b1af7f2b2867b962d
xoddus904 <eunlove904@gmail.com>
2020-12-05 02:53:39 +0900

    201205_ 1. 면접주요질문 제목 넘기기 2. 답변 작성하기 3. 커뮤니티에서 추천 버튼 누를 시, 추천 버튼 바꾸기 (유지는 안됨...)

--- app/src/main/java/com/example/youthwings/InterviewActivity2.java
+++ app/src/main/java/com/example/youthwings/InterviewActivity2.java
@@ -5,6 +5,8 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -28,28 +30,30 @@ public class InterviewActivity2 extends AppCompatActivity {
     ResponseListAdapter responseListAdapter;
     ArrayList<ResponseListItem> responseListItemArrayList;
 
+    ArrayList<InterviewListItem> interviewListItemArrayList;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interview2);
 
-
-        Intent intent = getIntent();
-        String interviewTitle = intent.getExtras().getString("interviewTitle");
-
+        //제목 가져오기
         textView = (TextView)findViewById(R.id.interview_title_detail);
-        textView.setText(interviewTitle);
 
+        //답변 리스트
         listView = (ListView)findViewById(R.id.response_listview);
         responseListItemArrayList = new ArrayList<ResponseListItem>();
 
         responseListItemArrayList.add( new ResponseListItem("닉네임1",2,"쓸말이 없다..."));
         responseListItemArrayList.add( new ResponseListItem("닉네임2",2,"가나다라마바사아자차카타파하 최대한 길게 써야하는데 무슨 말을 해야할까요 동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 이 기상과 이맘으로 충성을 다하여 괴로우나 즐거우나\n" +
                 " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        responseListItemArrayList.add( new ResponseListItem("닉네임3",2,"오호 오 ㅐ 기능이 안될까나.......뭐가 문제일까욥"));
+        responseListItemArrayList.add( new ResponseListItem("닉네임4",2,"왜이래애애ㅐ............"));
+        responseListItemArrayList.add( new ResponseListItem("닉네임5",2,"가갸거겨고교구규그기나냐너녀노뇨누뉴느니"));
 
         responseListAdapter = new ResponseListAdapter(InterviewActivity2.this, responseListItemArrayList);
         listView.setAdapter(responseListAdapter);
-
 
         initLayout();
     }
@@ -65,18 +69,36 @@ public class InterviewActivity2 extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //뒤로가기버튼
     }
 
-    //툴바
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { //글쓰기 버튼
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.toolbarmenu, menu);
+        menu.findItem(R.id.toolbar_help_button).setIcon(R.drawable.icon_edit);
+        return true;
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case android.R.id.home:
+            case android.R.id.home: {
                 finish();
-                return true;
+                break;
+            }
+            case R.id.toolbar_help_button: { //글쓰기 버튼인데 아이디는 도움말 버튼과 동일
+                // ***************************************
+                // 글쓰기 버튼 - 제목도 같이 넘김.
+                // ***************************************
+                Intent intent = new Intent(InterviewActivity2.this, InterWritingActivity.class);
+                intent.putExtra("interviewTitle",textView.getText().toString());
+                startActivity(intent);
+                break;
+            }
         }
         return super.onOptionsItemSelected(item);
     }
 
-    //답근 추천버튼
+
+    //답글 추천버튼
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id. btn_goodresponse:
